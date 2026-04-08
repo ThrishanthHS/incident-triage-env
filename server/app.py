c943a8577b7e3c94f490218bd885ee9c8edae111
Thrishanth HS <thrishanthhs@email.com>
2026-04-08 17:57:47 +0530

    Fix server/app.py with main() function

--- server/app.py
+++ server/app.py
@@ -1,3 +1,10 @@
 ﻿import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from server_main import app
+
+def main():
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=7860)
+
+if __name__ == '__main__':
+    main()
